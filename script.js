8a2a3399b9002f4e7a7becdba4a41b30e7dba7cc
unknown <George>
2020-02-24 14:45:36 +0400

    First commit

---
+++ script.js
@@ -0,0 +1,86 @@
+// console.log("Hello world");
+
+// var person = "John";
+// console.log(person);
+
+// person = "Nick";
+// console.log(person);
+
+// var first-Name = "Bob";
+
+// var LastName = "Doe";
+
+// let age = 30;
+
+// console.log(age);
+
+// age = 35;
+
+// console.log(age);
+
+// const city = "London";
+
+// console.log(city);
+
+// const str = "John is a developer";
+// console.log(str);
+// console.log(typeof str);
+
+// const num = "27.23";
+// console.log(num);
+// console.log(typeof num);
+
+// const num1 = 10;
+// const num2 = 20;
+// const bool = true;
+// console.log(typeof bool);
+
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// const b = null;
+// console.log(b);
+
+// const a = 5 / 5;
+// console.log(a);
+
+// const b = (5 + 5) * 5;
+// console.log(b);
+
+// const num1 = 10;
+// const num2 = "10";
+
+// const a = 10 * "20";
+// console.log(a);
+
+// const b = "Hello " + "there";
+// console.log(b);
+
+// const c = "a" + 10 + 15;
+// console.log(c);
+
+// const kid = "Alexis";
+// const gender = "Female";
+
+// if (gender == "Male") {
+//   console.log(kid + " is my son");
+// } else {
+//   console.log(kid + " is my daughter");
+// }
+
+// const prof = "engineer";
+
+// if (prof === "composer") {
+//   console.log(prof + " teaches students");
+// } else if (prof === "composer") {
+//   console.log(prof + " creates music");
+// } else {
+//   console.log("Professions do not match");
+// }
+
+// if (!5 === 5) {
+//   console.log("Condition is true");
+// } else {
+//   console.log("Condition is false");
+// }
